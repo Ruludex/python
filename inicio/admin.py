1b1b11fe6cf83341fa51323f0274e21a45286347
Ruludex <gastonghj@hotmail.com.ar>
2026-05-22 14:55:42 -0300

    11

--- inicio/admin.py
+++ inicio/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
-from .models import Post  
+from .models import Post, Comentario # 👈 Asegurate de importar Comentario
 
+# Seguro ya tenés registrado tu Post acá arriba:
 admin.site.register(Post)
+
+# 🔽 AGREGÁ ESTA LÍNEA ACÁ ABAJO 🔽
+admin.site.register(Comentario)
