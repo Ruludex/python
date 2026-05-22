e2e2cd54a98b34c2d85206faba1d01867e8d3ea7
Ruludex <gastonghj@hotmail.com.ar>
2026-05-22 02:19:00 -0300

    8

--- inicio/views.py
+++ inicio/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Post  # 👈 Importamos tus Posts para poder usarlos
 
-
-def inicio (request):
-    return render(request,'inicio.html',{})
+def inicio(request):
+    # 1. Traemos todos los posts ordenados por fecha (el más nuevo primero)
+    posts = Post.objects.all().order_by('-fecha_publicacion')
+    
+    # 2. Le pasamos esos posts al HTML metiéndolos en el diccionario
+    return render(request, 'inicio.html', {'posts': posts})
